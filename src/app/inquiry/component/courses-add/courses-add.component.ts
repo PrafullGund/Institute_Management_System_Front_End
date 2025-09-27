02f27334c79dacb64bc1d2d21f94a6c22f532aa0
Prafull <prafullgund99@gmail.com>
2025-09-27 17:18:45 +0530

    course add api done

--- src/app/inquiry/component/courses-add/courses-add.component.ts
+++ src/app/inquiry/component/courses-add/courses-add.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { CourseService } from 'src/app/service/course.service';
+import { MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-courses-add',
@@ -7,24 +9,41 @@ import { CourseService } from 'src/app/service/course.service';
   styleUrls: ['./courses-add.component.scss']
 })
 export class CoursesAddComponent {
- courseData = {
-    courseName: '',
-    description: '',
-    courseFees: 0
-  };
+  courseFrom!:FormGroup;
 
-  constructor(private courseService: CourseService) { }
+  constructor(
+   private formBuilder:FormBuilder,
+   private coursesService:CourseService,
+   private dialogRef:MatDialogRef<CoursesAddComponent>,
+   @Inject(MAT_DIALOG_DATA) public data: any
+  ) { 
+    this.courseFrom=this.formBuilder.group({
+      courseName:[''],
+      courseFees:[''],
+      courseDuration:[''],
+      courseMode:[''],
+      description:[''],
+      typeName:[''],
+      typeDescription:['']
+    })
+  }
 
-  addCourse() {
-    // this.courseService.addCourse(this.courseData).subscribe({
-    //   next: (res) => {
-    //     console.log('Course added', res);
-    //     // Close modal manually
-    //     const modalEl = document.getElementById('addCourseModal');
-    //     const modal = bootstrap.Modal.getInstance(modalEl);
-    //     modal?.hide();
-    //   },
-    //   error: (err) => console.error('Error adding course', err)
-    // });
+  ngOnInit(){
+
+  }
+
+  saveCourse() {
+    if(this.courseFrom.valid){
+      this.coursesService.postAllCourse(this.courseFrom.value).subscribe({
+        next:(val:any)=>{
+          this.dialogRef.close(true);
+        },
+        error:(err:any)=>{
+          console.error('Error adding course:', err);
+        }
+      });
+    }else{
+       console.log('Form is invalid');
+    }
   }
 }
